2aca7de4ce50ce7f0146cdde494ceff552f0b3a7
Niel22 <niel2264@gmail.com>
2024-12-31 13:43:53 +0100

    CRUD Implemented

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -11,9 +11,16 @@ function save(req, res)
     }
 
     models.Post.create(post).then(result => {
-        res.status(201).json({
-            message: "Post created successfully",
-            post: result
+        if(result){
+
+            return res.status(200).json({
+                message: "Post Created",
+                post: result
+            })
+        }
+        
+        return res.status(400).json({
+            message: "Problem creating Post"
         })
     }).catch(error => {
         res.status(500).json({
@@ -28,7 +35,15 @@ function show(req, res)
     const id = req.params.id;
 
     models.Post.findByPk(id).then(result => {
-        res.status(200).json(result)
+        if(result){
+
+            return res.status(200).json(result)
+        }
+        
+        return res.status(400).json({
+            message: "Post not found"
+        })
+
     }).catch(error => {
         res.status(500).json({
             message: 'Something went wrong',
@@ -40,7 +55,15 @@ function show(req, res)
 function index(req, res)
 {
     models.Post.findAll().then(result => {
-        res.status(200).json(result);
+        if(result){
+
+            return res.status(200).json(result)
+        }
+        
+        return res.status(400).json({
+            message: "No Post Found"
+        })
+
     }).catch(error => {
         res.status(500).json({
             message: 'Something went wrong',
@@ -49,8 +72,82 @@ function index(req, res)
     })
 }
 
+function update(req, res)
+{
+    const id = req.params.id;
+
+    const updatedPost = {
+        title: req.body.title,
+        content: req.body.content,
+        imageUrl: req.body.imageUrl,
+        categoryId: req.body.categoryId,
+    }
+
+    const userId = 1;
+
+    models.Post.update(updatedPost, {
+        where: {
+            id: id,
+            userId: userId
+        }
+    }).then(result => {
+
+        if(result){
+
+            return res.status(200).json({
+                message: "Post Updated",
+                post: result
+            });
+        }
+
+        return res.status(400).json({
+            message: "Post Cannot be updated"
+        })
+
+    }).catch(error => {
+        res.status(500).json({
+            message: "something went wrong",
+            error: error
+        });
+    })
+}
+
+function destroy(req, res)
+{
+    const id = req.params.id;
+    const userId = 1;
+
+    models.Post.destroy({
+        where: {
+            id: id,
+            userId: userId
+        }
+    }).then(result => {
+        if(result){
+
+            return res.status(200).json({
+                message: "Post Deleted",
+                post: result
+            });
+        }
+
+        return res.status(400).json({
+            message: "post cannot be deleted"
+        })
+
+
+    }).catch(error => {
+        res.status(500).json({
+            message: "something went wrong",
+            error: error
+        });
+    })
+}
+
 module.exports = {
     save: save,
     show: show,
-    index: index
+    index: index,
+    update: update,
+    destroy: destroy
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,9 @@ const PostController = require('../controllers/post.controller');
 const router = express.Router();
 
 router.post('/', PostController.save);
-router.get('/:id', PostController.show);
 router.get('/', PostController.index);
+router.get('/:id', PostController.show);
+router.patch('/:id', PostController.update);
+router.delete('/:id', PostController.destroy);
 
 module.exports = router;
