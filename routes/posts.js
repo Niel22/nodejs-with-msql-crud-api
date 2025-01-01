a58c3140f5afeb8a5fdc4b3e83902893d03ffb58
Niel22 <niel2264@gmail.com>
2025-01-01 21:15:51 +0100

    Implemented middleware

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const PostController = require('../controllers/post.controller');
+const checkAuthMiddleware = require('../middleware/checkAuth');
 
 const router = express.Router();
 
-router.post('/', PostController.save);
+
+router.post('/', checkAuthMiddleware.checkAuth, PostController.save);
 router.get('/', PostController.index);
 router.get('/:id', PostController.show);
-router.patch('/:id', PostController.update);
-router.delete('/:id', PostController.destroy);
+router.patch('/:id', checkAuthMiddleware.checkAuth, PostController.update);
+router.delete('/:id', checkAuthMiddleware.checkAuth, PostController.destroy);
 
 module.exports = router;
